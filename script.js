0ae05c32de3ae3b9b2588db1b68eb0f7a9fab98e
Emerson Almeida Souza <emerson.souza@encora.com>
2025-03-19 13:28:11 -0300

    Adição de projetos e criação de menu de hamburguer

--- script.js
+++ script.js
@@ -104,3 +104,13 @@ function highlightCurrentSection() {
         }
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        menu.classList.toggle('open');
+    });
+});
